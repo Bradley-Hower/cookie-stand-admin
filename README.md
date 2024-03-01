dbe1fc2bb5a9994887613fa8d09ff71e2e4e3378
Bradley Hower <bradleyhower@proton.me>
2024-02-29 16:48:23 -0800

    class-39

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# LAB - Class 37 & 38
+# LAB - Class 37, 38, & 39
 
 ## Project: Cookie Stand Admin - Leveling up Front End with React, Next.js & TailwindCSS
 
@@ -8,7 +8,9 @@
 
 In this lab, the webpage created is made to best emulate the mockup screenshot found at [Cookie Stand Admin Version 1](https://codefellows.github.io/seattle-code-python-401d24/class-37/lab/cookie-stand-admin-version-1.png). Webpage is made using Next.js, which is a framework that builds on top of TailwindsCSS and React.
 
-Second update, added ability to add rows, as well as improve formatting to match second screenshot mockup found at [Cookie Stand Admin Version 2](https://codefellows.github.io/code-401-python-guide/curriculum/class-38/lab/cookie-stand-admin-version-2.png). Components are separated into their respective page.
+Second version update, added ability to add rows, as well as improve formatting to match second screenshot mockup found at [Cookie Stand Admin Version 2](https://codefellows.github.io/code-401-python-guide/curriculum/class-38/lab/cookie-stand-admin-version-2.png). Components are separated into their respective page.
+
+Third version update. Added conditional rendering for login. Login to look according to [Cookie Stand Admin Login](https://codefellows.github.io/code-401-python-guide/curriculum/class-39/lab/cookie-stand-admin-login.png) mockup. Main page updated according to [Cookie Stand Admin Version 3](https://codefellows.github.io/code-401-python-guide/curriculum/class-39/lab/cookie-stand-admin-version-3.png) mockup.
 
 ## Getting Started
 
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,145 @@
-import { useState } from 'react';
+import { useAuth } from '@/contexts/auth';
+import useResource from '../hooks/useResource'
 import { hours } from '../data';
 import Head from '@/components/head';
 import Header from '@/components/header';
-import CreateForm from '@/components/create-form';
-import ReportTable from '@/components/report-table';
 import Footer from '@/components/footer';
 
 export default function Home() {
-  const [standReports, setStandReports] = useState([]);
-  
-  function handleCreate(standInfo){
-    setStandReports([...standReports, standInfo]);
-  };
-
-  const rowCount = standReports.length;
+  const { user, login } = useAuth();
 
   return(
     <div>
-      <Head />     
+      <Head /> 
+      {user ? 
+              <> 
+                <h1>Logged in: {user.email}</h1>
+                <CookieStandAdmin />
+              </>
+            :
+                <LoginForm onLogin={login} />
+      }    
       <Header />
-
-      <main>
-        <CreateForm onCreate={handleCreate} />
-        <ReportTable reports={standReports} hours={hours}/>
-      </main>
       <Footer rowCount={rowCount}/>
     </div>
+  );
+}
+
+function CookieStandAdmin() {
+  const { resources, deleteResources } = useResource();
+
+  return (
+    <>
+      <CookieStandForm />
+      <CookieStandTable stand={resources || []} deteleStand={deleteResources} hours={hours}/>
+    </>
+  )
+}
+
+function CookieStandForm() {
+
+  const { user } = useAuth();
+  const { createResource } = useResource();
+
+  function handleSubmit(event) {
+      event.preventDefault();
+      const info = {
+          location: event.target.location.value,
+          minimum_customers_per_hour: parseInt(event.target.minimum.value),
+          maximum_customers_per_hour: parseInt(event.target.maximum.value),
+          average_cookies_per_sale: parseFloat(event.target.average.value),
+          owner: user.id,
+      };
+      createResource(info);
+
+  }
+
+  return (
+      <form onSubmit={handleSubmit}>
+          <fieldset>
+              <legend>Create Cookie Stand</legend>
+              <input placeholder='location' name='location' />
+              <input placeholder='minimum' name='minimum' />
+              <input placeholder='maximum' name='maximum' />
+              <input placeholder='average' name='average' />
+              <button>create</button>
+          </fieldset>
+      </form>
+  );
+}
+
+function CookieStandTable({ stands, deleteStand, hours }) {
+
+  const headers = ['Location', ...{hours}, 'Totals'];
+  
+  return (
+      <table className="w-2/3 mx-auto my-8 bg-green-300 rounded-lg">
+        <HeaderRow headerValues={headers} />
+          <tbody>
+              {stands.map(stand => (
+
+                  <CookieStandRow key={stand.id} info={stand} deleteStand={deleteStand} />
+              ))}
+          </tbody>
+      </table>
+  );
+}
+
+function HeaderRow({ headerValues }){
+  return (
+    <thead className="bg-green-500">
+      <tr>
+        {headerValues.map((header, index) => {
+          let className = "";
+          if (index === 0){
+            className = "rounded-tl";
+          } else if (index === headerValues.length - 1){
+            className = "rounded-tr";
+          }
+          return <th className={className} key={header}>{header}</th>
+        })}
+      </tr>
+    </thead>
+  )
+}
+
+function CookieStandRow({ info, deleteStand }) {
+
+  function clickHandler() {
+      deleteStand(info.id);
+  }
+
+  if (info.hourly_sales.length == 0) {
+      // bunk data
+      info.hourly_sales = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  }
+
+  return (
+      <tr className="odd:bg-green-400">
+          <td>{info.location} <button onClick={clickHandler}>[x]</button></td>
+          {info.hourly_sales.map((slot,index) => <td className="pl-4 border border-green-900" key={index}>{slot}</td>)}
+          <td>{info.hourly_sales.reduce((num, sum) => num + sum, 0)}</td>
+      </tr>
+  );
+}
+
+function LoginForm({ onLogin }) {
+
+  async function handleSubmit(event) {
+      event.preventDefault();
+      onLogin(event.target.username.value, event.target.password.value);
+  }
+
+  return (
+      <form onSubmit={handleSubmit}>
+          <fieldset autoComplete='off'>
+              <legend>Log In</legend>
+              <label htmlFor="username">Username</label>
+              <input name="username" />
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" />
+              <button>Log In</button>
+          </fieldset>
+      </form>
   );
 }
